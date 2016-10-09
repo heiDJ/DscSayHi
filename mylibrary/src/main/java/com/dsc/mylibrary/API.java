b8c372b110903cd4c298227c2d2594bf81966a2a
heiDJ <woshidsc77@163.com>
2016-10-09 11:45:01 +0800

    qqq

---
+++ mylibrary/src/main/java/com/dsc/mylibrary/API.java
@@ -0,0 +1,12 @@
+package com.dsc.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by staff on 2016/10/9.
+ */
+public class API {
+    public static void sayHi(){
+        Log.e("mylibrary","hi~~~~~~~~~~");
+    }
+}
